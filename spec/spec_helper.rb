cd179b0b13fa0ec76902439f4baf83ef1fe0b27b
Hilde Vestol <hildevestol@users.noreply.github.com>
2025-08-18 14:48:44 +0000

    Match vcr on body

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,8 +31,14 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes' # Directory to store recorded interactions
   config.hook_into :webmock # or :faraday, etc., depending on your chosen library
+
+  # Match on method, uri AND body
+  # to ensure it fails if e.g. a request to AI
+  # changes in code, then we want to re-record the result
+  config.default_cassette_options[:match_requests_on] = [:method, :uri, :body] # default [:method, :uri]
+
+  # Filter out sensitive information
   config.filter_sensitive_data('<OPENAI_API_KEY>') do |interaction|
-    # Example: Replace AWS access key from environment variables
     ENV['OPENAI_API_KEY']
   end
 end
